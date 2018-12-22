d31e9ccf065e8deb53a2cd9a0e1a3046ffc16eee
cochiletSerban <cochiletSerban@gmail.com>
2018-12-22 15:35:51 +0200

    finished initial mokeup

--- public/tab-group-component/tab-group.js
+++ public/tab-group-component/tab-group.js
@@ -1,7 +1,7 @@
 function tabGroup () {
   $('.tabs').tabs()
   $('.collapsible').collapsible()
-
+  $('select').formSelect()
   // fixes materialize slider tumb react/vue bug
   let arrayOfSliders = document.querySelectorAll('input[type=range]')
   M.Range.init(arrayOfSliders)
@@ -9,9 +9,44 @@ function tabGroup () {
   $('#picker').spectrum({
     showButtons: false
   })
+  $('#picker1').spectrum({
+    showButtons: false
+  })
+  $('#picker2').spectrum({
+    showButtons: false
+  })
+  $('#picker3').spectrum({
+    showButtons: false
+  })
+  $('#picker4').spectrum({
+    showButtons: false
+  })
+  $('#picker5').spectrum({
+    showButtons: false
+  })
 
-  $('#picker').on("move.spectrum", function (e, color) {
-    color.toHexString(); // #ff0000
-    console.log(color.toHexString());
-    })
+  $('#picker').on('move.spectrum', function (e, color) {
+    color.toHexString() // #ff0000
+    console.log(color.toHexString())
+  })
+  $('#picker1').on('move.spectrum', function (e, color) {
+    color.toHexString() // #ff0000
+    console.log(color.toHexString())
+  })
+  $('#picker2').on('move.spectrum', function (e, color) {
+    color.toHexString() // #ff0000
+    console.log(color.toHexString())
+  })
+  $('#picker3').on('move.spectrum', function (e, color) {
+    color.toHexString() // #ff0000
+    console.log(color.toHexString())
+  })
+  $('#picker4').on('move.spectrum', function (e, color) {
+    color.toHexString() // #ff0000
+    console.log(color.toHexString())
+  })
+  $('#picker5').on('move.spectrum', function (e, color) {
+    color.toHexString() // #ff0000
+    console.log(color.toHexString())
+  })
 }
